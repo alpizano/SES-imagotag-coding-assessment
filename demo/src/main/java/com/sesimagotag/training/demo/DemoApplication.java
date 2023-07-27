8c95684801020f356d9d01fc1931e2b5d4d4b3ce
alpizano <dev.iation2099@gmail.com>
2023-07-27 14:32:09 -0500

    added comment to explain SpringBootApplication annotation

--- demo/src/main/java/com/sesimagotag/training/demo/DemoApplication.java
+++ demo/src/main/java/com/sesimagotag/training/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.sesimagotag.training.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Annotation that adds Spring Boot @Configuration, @EnableAutoConfiguration, and @ComponentScan annotations.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
