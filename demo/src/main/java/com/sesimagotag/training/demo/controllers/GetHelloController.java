232b5b5903836b72b8194b0f8749b0873c50c0fc
alpizano <dev.iation2099@gmail.com>
2023-07-27 14:31:10 -0500

    created GET hello controller from request.http instructions

--- demo/src/main/java/com/sesimagotag/training/demo/controllers/GetHelloController.java
+++ demo/src/main/java/com/sesimagotag/training/demo/controllers/GetHelloController.java
@@ -1,15 +1,16 @@
 package com.sesimagotag.training.demo.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RestControllerGetHello {
+public class GetHelloController {
 
     /**
      * GET hello request
      */
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getHello() {
         return "Greetings from Spring Boot!";
     }
